c01c165b097974779cb99391e4858c2130e93c69
Vinura Nanayakkara <vinurananayakkara24@gmail.com>
2025-07-22 08:53:56 +0530

    .

--- vite-project/main.py
+++ vite-project/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS to allow frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
